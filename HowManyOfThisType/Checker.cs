c5489daa242ede3b3f80e8964a5df11c9436d4ea
Francisco Anjos <a22204855@alunos.ulht.pt>
2023-06-02 10:18:14 +0100

    add Function to it

--- HowManyOfThisType/Checker.cs
+++ HowManyOfThisType/Checker.cs
@@ -6,7 +6,18 @@ namespace HowManyOfThisType
     {
         public static class Checker
         {
-            
+            public static int HowManyOfType<T>(IEnumerable<object> items)
+            {   
+                int count = 0;
+                foreach (object item in items)
+                {
+                    if (item is T)
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            }
         }
     }
 }
