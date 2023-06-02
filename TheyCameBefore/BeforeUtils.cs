c189b051e1ef3cc97a0ac9693238c3033f7b53a5
Francisco Anjos <fcariaanjos@gmail.com>
2023-06-02 10:44:14 +0100

    Add Its function

--- TheyCameBefore/BeforeUtils.cs
+++ TheyCameBefore/BeforeUtils.cs
@@ -4,6 +4,17 @@ namespace TheyCameBefore
 {
     public static class BeforeUtils
     {
-
+          public static IEnumerable<T> GetTheOnesBefore<T>(IEnumerable<T> items, 
+        T item)
+            where T : IComparable<T>
+        {
+            foreach (T currentItem in items)
+            {
+                if (currentItem.CompareTo(item) < 0)
+                {
+                    yield return currentItem;
+                }             
+            }
+        }
     }
 }
