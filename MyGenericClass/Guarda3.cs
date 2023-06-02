6e675612e539b0c7ad41722297f8d01fb76e3ce4
Francisco Anjos <fcariaanjos@gmail.com>
2023-06-02 10:53:06 +0100

    Added Guarda3 and built its constructor

---
+++ MyGenericClass/Guarda3.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace MyGenericClass
+{
+    public class Guarda3<T>
+    {
+        private T[] items;
+
+        public Guarda3()
+        {
+            items = new T[3];
+        }
+
+        
+    }
+}
